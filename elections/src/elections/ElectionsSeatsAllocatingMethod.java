5239c44ed6199d358bb40e4ebb0931fa88af60ad
Jakub Molinski <jakub@molinski.dev>
2020-06-10 19:59:00 +0200

    Added mandate allocation methods

--- elections/src/elections/ElectionsSeatsAllocatingMethod.java
+++ elections/src/elections/ElectionsSeatsAllocatingMethod.java
@@ -1,6 +1,8 @@
 package elections;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 abstract public class ElectionsSeatsAllocatingMethod {
     protected final HashMap<Constituency, ConstituencyResults> resultsByConstituency;
@@ -36,7 +38,52 @@ abstract public class ElectionsSeatsAllocatingMethod {
     protected abstract String methodName();
 }
 
-class DHondtMethod extends ElectionsSeatsAllocatingMethod {
+abstract class QuotientBasedSeatsAllocatingMethod extends ElectionsSeatsAllocatingMethod {
+    protected QuotientBasedSeatsAllocatingMethod(Constituency[] constituencies, Party[] parties) {
+        super(constituencies, parties);
+    }
+
+    protected HashMap<Party, Integer> calculateMandatesByParty(HashMap<Party, Integer> votesPerParty,
+                                                               int mandatesCount) {
+        var mandatesByParty = new HashMap<Party, Integer>();
+        for (Party p : votesPerParty.keySet()) {
+            mandatesByParty.put(p, 0);
+        }
+
+        for (int i = 0; i < mandatesCount; i++) {
+            var candidatesForMandate = getCandidatesForNextMandate(mandatesByParty, votesPerParty);
+            var party = candidatesForMandate[(new Random().nextInt(candidatesForMandate.length))];
+            mandatesByParty.put(party, mandatesByParty.get(party) + 1);
+        }
+
+        return mandatesByParty;
+    }
+
+    private Party[] getCandidatesForNextMandate(HashMap<Party, Integer> mandates, HashMap<Party, Integer> votes) {
+        double maxQuotient = -1;
+        for (Party party : mandates.keySet()) {
+            double partyQuotient = getQuotient(mandates.get(party), votes.get(party));
+            if (partyQuotient < maxQuotient) {
+                maxQuotient = partyQuotient;
+            }
+        }
+
+        var candidates = new ArrayList<Party>();
+        for (Party party : mandates.keySet()) {
+            double partyQuotient = (double) votes.get(party) / (double) (mandates.get(party) + 1);
+            if (Double.compare(partyQuotient, maxQuotient) == 0) {
+                candidates.add(party);
+            }
+        }
+
+        return candidates.toArray(Party[]::new);
+    }
+
+    protected abstract double getQuotient(double mandates, double votes);
+}
+
+
+class DHondtMethod extends QuotientBasedSeatsAllocatingMethod {
     public DHondtMethod(Constituency[] constituencies, Party[] parties) {
         super(constituencies, parties);
     }
@@ -45,14 +92,14 @@ class DHondtMethod extends ElectionsSeatsAllocatingMethod {
         return "D'Hondt Method";
     }
 
-    protected HashMap<Party, Integer> calculateMandatesByParty(HashMap<Party, Integer> votesPerParty,
-                                                               int mandatesCount) {
-        return null;  // TODO
+    @Override
+    protected double getQuotient(double mandates, double votes) {
+        return votes / (mandates + 1);
     }
 }
 
 
-class SainteLagueMethod extends ElectionsSeatsAllocatingMethod {
+class SainteLagueMethod extends QuotientBasedSeatsAllocatingMethod {
     public SainteLagueMethod(Constituency[] constituencies, Party[] parties) {
         super(constituencies, parties);
     }
@@ -61,9 +108,9 @@ class SainteLagueMethod extends ElectionsSeatsAllocatingMethod {
         return "Sainte-Lague Method";
     }
 
-    protected HashMap<Party, Integer> calculateMandatesByParty(HashMap<Party, Integer> votesPerParty,
-                                                               int mandatesCount) {
-        return null;  // TODO
+    @Override
+    protected double getQuotient(double mandates, double votes) {
+        return votes / (2 * mandates + 1);
     }
 }
 
@@ -79,6 +126,47 @@ class HareNiemeyerMethod extends ElectionsSeatsAllocatingMethod {
 
     protected HashMap<Party, Integer> calculateMandatesByParty(HashMap<Party, Integer> votesPerParty,
                                                                int mandatesCount) {
-        return null;  // TODO
+        var partyScores = getPartyScores(votesPerParty, mandatesCount);
+        var mandatesByParty = new HashMap<Party, Integer>();
+
+        int remainingMandates = mandatesCount;
+        for (Party party : partyScores.keySet()) {
+            int partyMandates = partyScores.get(party).intValue();
+            mandatesByParty.put(party, partyMandates);
+            partyScores.put(party, partyScores.get(party) - partyMandates);
+            remainingMandates -= partyMandates;
+        }
+
+        while (remainingMandates > 0) {
+            double maxScore = -1;
+            Party nextPartyToGetMandate = null;
+
+            for (Party party : partyScores.keySet()) {
+                if (partyScores.get(party) > maxScore) {
+                    maxScore = partyScores.get(party);
+                    nextPartyToGetMandate = party;
+                }
+            }
+
+            mandatesByParty.put(nextPartyToGetMandate, mandatesByParty.get(nextPartyToGetMandate) + 1);
+            partyScores.remove(nextPartyToGetMandate);
+            remainingMandates -= 1;
+        }
+
+        return mandatesByParty;
+    }
+
+    private HashMap<Party, Double> getPartyScores(HashMap<Party, Integer> votesPerParty, int mandatesCount) {
+        int totalVotes = 0;
+        for (int partyVotes : votesPerParty.values()) {
+            totalVotes += partyVotes;
+        }
+
+        var partyScores = new HashMap<Party, Double>();
+        for (Party party : votesPerParty.keySet()) {
+            partyScores.put(party, (double) (votesPerParty.get(party) * mandatesCount) / (double) totalVotes);
+        }
+
+        return partyScores;
     }
 }
